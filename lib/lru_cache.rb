acd2b367384224112d28b8fed80367aef2b0ce45
Phan Le & Whitney Schaefer <pair+phan+whitney@pivotallabs.com>
2012-12-18 13:15:12 -0800

    Add support for ttl and clean up when set the same key

--- lib/lru_cache.rb
+++ lib/lru_cache.rb
@@ -5,17 +5,23 @@ class LruCache
     @lru_list = DoubleLinkedList.new
   end
 
-  def put(k,v)
+  def put(k,v, ttl=nil)
+    remove_key_if_present(k)
     if @items.size == @capacity
       evist_last_recently_used
     end
-    node = @lru_list.add(k, v)
-    @items[k] = node
+    expired_at = ttl ?  Time.now + ttl : nil
+    new_node = @lru_list.add(k, v, expired_at)
+    @items[k] = new_node
   end
 
   def get(k)
     node = @items[k]
     return nil unless node
+    if node.expired?
+      remove_key_if_present(k)
+      return nil
+    end
     @lru_list.change_mru(node)
     return node.data
   end
@@ -24,6 +30,13 @@ class LruCache
   def evist_last_recently_used
     @items[@lru_list.lru.key] = nil
     @lru_list.remove_lru
+  end
+
+  def remove_key_if_present(k)
+    if existing_node = @items[k]
+      @lru_list.remove_node(existing_node)
+      @items.delete(k)
+    end
   end
 
   class DoubleLinkedList
@@ -36,11 +49,11 @@ class LruCache
 
     def change_mru(node)
       remove_node(node)
-      add(node.key, node.data)
+      add(node.key, node.data, node.expired_at)
     end
 
-    def add(k, v)
-      node = Node.new(k, v, @mru, nil)
+    def add(k, v, expired_at)
+      node = Node.new(k, v, @mru, nil, expired_at)
       @lru = node unless lru
       @mru.older = node if mru
       @mru = node
@@ -50,7 +63,6 @@ class LruCache
       remove_node(lru)
     end
 
-    private
     def remove_node(node)
       node.newer.older = node.older if node.newer
       node.older.newer = node.newer if node.older
@@ -59,13 +71,19 @@ class LruCache
   end
 
   class Node
-    attr_accessor :older, :newer, :data, :key
+    attr_accessor :older, :newer, :data, :key, :expired_at
 
-    def initialize(key, data, older, newer)
+    def initialize(key, data, older, newer, expired_at)
       @key  = key
       @data = data
       @older = older
       @newer = newer
+      @expired_at = expired_at
+    end
+
+    def expired?
+      return false unless @expired_at
+      return Time.now >= @expired_at
     end
   end
 end
--- spec/lru_cache_spec.rb
+++ spec/lru_cache_spec.rb
@@ -1,4 +1,6 @@
-require "lru_cache"
+require 'lru_cache'
+require 'timecop'
+
 describe "lru cache" do
   let(:cache) { LruCache.new(2)  }
 
@@ -36,9 +38,26 @@ describe "lru cache" do
     cache.get("k3").should == "v3"
   end
 
-  it 'return nil for key it does not know' do
+  it 'returns nil for key it does not know' do
     cache.put("k1", "v1")
     cache.put("k2", "v2")
     cache.get('k3').should be_nil
+  end
+
+  it 'allows overriding a key and evicts the right one' do
+    cache.put("k1", "v1")
+    cache.put("k2", "v2")
+    cache.put("k2", "new v2")
+    cache.get('k2').should == 'new v2'
+    cache.get('k1').should == 'v1'
+  end
+
+  it 'supports ttl' do
+    now = Time.now
+    cache.put("k1", "v1", 2)
+    cache.get("k1").should == 'v1'
+    Timecop.travel(now + 3) do
+      cache.get("k1").should be_nil
+    end
   end
 end
